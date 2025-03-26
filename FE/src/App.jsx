031941ec84616c6d4810c86e0d9b9c37b1cc2296
programmerpadama <itprogrammerpadama@gmail.com>
2025-03-26 13:57:01 +0700

    NEW SETUP

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -3,7 +3,8 @@ import { router } from "./components/routes/Routes";
 import { ApiUrl } from "./context/Urlapi";
 
 function App() {
-  const baseUrl = "http://192.168.9.192:3001/api";
+  // const baseUrl = "http://192.168.9.192:3001/api";
+  const baseUrl = "http://192.168.9.208:3001/api";
   // const baseUrl = "http://localhost:3000/api";
   return (
     <>
--- FE/src/context/Urlapi.js
+++ FE/src/context/Urlapi.js
@@ -1,4 +1,5 @@
 import { createContext } from "react";
 
-export const ApiUrl = createContext("http://192.168.9.192:3001/api");
+// export const ApiUrl = createContext("http://192.168.9.192:3001/api");
+export const ApiUrl = createContext("http://192.168.9.208:3001/api");
 // export const ApiUrl = createContext("http://localhost:3000/api");
